c67ef6b75aaca105715143e89d9134046362c8ca
Hrishikesh Yadav <hrishikeshyadav92@gmail.com>
2024-04-21 12:12:46 +0530

    Signup functionality implemented

--- Backend/Controllers/auth.controller.js
+++ Backend/Controllers/auth.controller.js
@@ -1,9 +1,56 @@
+import User from "../Models/user.model"
+import bcryptjs from 'bcryptjs'
+
 export const signup = async(req,res)=>{
     try{
-        const {fullname, username, password, confirmPassword, gender} = req.body
-        res.send('signup user')
-        console.log('signup User')
-    }catch{}
+        const {fullName, username, password, confirmPassword, gender} = req.body
+
+        if(password !== confirmPassword) {
+            return res.status(400).json({error: "Password don't match" })
+        }
+
+        const user = await User.findOne({username})
+
+        if(user){
+            return res.status(400).json({error: "Username already exists"})
+        }
+
+        // HASH PASSWORD HERE
+        const salt = await bcryptjs.genSalt(10)
+        const hashedPassword = await bcryptjs.hash(password,salt)
+
+        // https://avatar-placeholder.iran.liara.run/
+
+        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`
+        const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`
+
+        const newUser = new User({
+            fullName : fullName,
+            username : username,
+            password : hashedPassword,
+            gender: gender,
+            profilePic : gender === 'male' ? boyProfilePic : girlProfilePic
+        })
+
+        
+        if(newUser){
+            await newUser.save()
+
+            res.status(201).json({
+                _id: newUser._id,
+                fullName : newUser.fullName,
+                username: newUser.username,
+                profilePic: newUser.profilePic
+            })
+        } else{
+            res.status(400).json({error: "Invalid user data"})
+        }
+
+        
+    } catch(error){
+        console.log("Error in signup controller", error.message)
+        res.status(500).json({error:"Internal server Error"})
+    }
 }
 
 export const login = (req,res)=>{
--- Backend/server.js
+++ Backend/server.js
@@ -3,15 +3,21 @@ import dotenv from 'dotenv'
 import authRoutes from './Routes/auth.routes.js'
 import connectToMongoDB from './DB/connecttoMongoDB.js'
 
+// Variables
 const app = express()
-dotenv.config()
 const PORT = process.env.PORT || 5000
 
-app.get('/',(req,res)=>{
-    res.send('Hello world')
-})
+dotenv.config()
 
+//Middlewares
+app.use(express.json()) // to parse the incoming requests with json payloads
 app.use('/api/auth', authRoutes)
+
+// Test routes
+// app.get('/',(req,res)=>{
+//     res.send('Hello world')
+// })
+
 
 app.listen(PORT, ()=> {
     connectToMongoDB()
