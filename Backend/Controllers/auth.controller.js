509bc368aaa7d774d2ea9a0ec10273d230a1f378
Hrishikesh1 Yadav <Hrishikesh1.Yadav@ril.com>
2024-04-21 11:18:57 +0530

    Connected to mongoDB database

--- Backend/Controllers/auth.controller.js
+++ Backend/Controllers/auth.controller.js
@@ -15,4 +15,4 @@ export const logout = (req,res)=>{
     res.send('logout user')
 
     console.log('logout User')
-}
+} 
--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import authRoutes from './Routes/auth.routes.js'
-dotenv.config()
+import connectToMongoDB from './DB/connecttoMongoDB.js'
 
 const app = express()
+dotenv.config()
 const PORT = process.env.PORT || 5000
 
 app.get('/',(req,res)=>{
@@ -12,4 +13,7 @@ app.get('/',(req,res)=>{
 
 app.use('/api/auth', authRoutes)
 
-app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`))  
+app.listen(PORT, ()=> {
+    connectToMongoDB()
+    console.log(`Server running on port ${PORT}`) 
+})              
