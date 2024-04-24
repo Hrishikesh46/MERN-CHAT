4970e08a74f9a3f15076c41c5d62e0a5b0531b5b
Hrishikesh Yadav <hrishikeshyadav92@gmail.com>
2024-04-24 10:22:36 +0530

    Get messages functionality implemented

--- Backend/Routes/message.routes.js
+++ Backend/Routes/message.routes.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { sendMessage } from '../Controllers/message.controller.js'
+import { getMessages, sendMessage } from '../Controllers/message.controller.js'
 import protectedRoute from '../Middleware/protectRoute.js'
 
 const router = express.Router()
 
+router.get('/:id', protectedRoute, getMessages)
 router.post('/send/:id', protectedRoute,sendMessage)
 
 export default router
