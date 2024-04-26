48a4e48f93b9b72bba3da359fba08805d439d300
Hrishikesh Yadav <hrishikeshyadav92@gmail.com>
2024-04-26 18:56:10 +0530

    auth context logic implemented

--- Frontend/src/Hooks/useSignup.js
+++ Frontend/src/Hooks/useSignup.js
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import toast from "react-hot-toast";
+import { useAuthContext } from "../Context/AuthContext.jsx";
 
 const useSignup = () => {
   const [loading, setLoading] = useState(false);
+  const { setAuthUser } = useAuthContext();
 
   const signup = async ({
     fullName,
@@ -34,6 +36,15 @@ const useSignup = () => {
         }),
       });
       const data = await res.json();
+
+      if (data.error) {
+        throw new Error(data.error);
+      }
+
+      // localstorage
+      localStorage.setItem("chat-user", JSON.stringify(data));
+      // context
+      setAuthUser(data);
 
       console.log(data);
     } catch (error) {
