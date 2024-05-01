b6818cd0baf6b285c0a1a4fbd7ace142b9813dc4
Hrishikesh Yadav <hrishikeshyadav92@gmail.com>
2024-05-01 13:04:11 +0530

    Deployment

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import authRoutes from "./Routes/auth.routes.js";
 import messageRoutes from "./Routes/message.routes.js";
@@ -13,6 +14,8 @@ import connectToMongoDB from "./DB/connecttoMongoDB.js";
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 dotenv.config();
 
 //Middlewares
@@ -22,6 +25,12 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 // Test routes
 // app.get('/',(req,res)=>{
