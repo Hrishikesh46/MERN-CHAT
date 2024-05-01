71a8b3844657094ea089c52f55a2cbc681722a1f
Hrishikesh Yadav <hrishikeshyadav92@gmail.com>
2024-05-01 14:03:19 +0530

    Socket URL updated

--- Frontend/src/Context/SocketContext.jsx
+++ Frontend/src/Context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:5000", {
+      const socket = io("https://mern-chat-guqd.onrender.com/", {
         query: {
           userId: authUser._id,
         },
